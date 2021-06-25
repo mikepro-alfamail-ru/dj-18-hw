4c1e0a6755ed971cef258f834662f3158c63de21
Mikhail Prozorov <mikepro@alfamail.ru>
2021-06-25 18:09:32 +0300

    pep8

--- drf/3.1-drf-intro/simple_crud/measurements/models.py
+++ drf/3.1-drf-intro/simple_crud/measurements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class TimestampFields(models.Model):
     '''
     abstract = True означает создание абстрактного класса
@@ -20,16 +21,13 @@ class TimestampFields(models.Model):
 
 class Project(TimestampFields):
     """Объект на котором проводят измерения."""
-
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
 
 
-
 class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
-
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     image = models.ImageField(
@@ -39,4 +37,3 @@ class Measurement(TimestampFields):
         max_length=10000,
         null=True
     )
-
